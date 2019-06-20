2a97f01c2664786af7372efc33efefdfb4b35c26
League Student <league@WTS9.attlocal.net>
2019-06-20 14:27:58 -0700

    hi
    hi

--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -17,16 +17,20 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		
 	
 	// 1. Set a password in a String variable
-	
+	String a="12345678910";
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+	String b=JOptionPane.showInputDialog("Write a secret message below:");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+	String c=JOptionPane.showInputDialog(null, "You can see the secret message if you can guess the password!");
 	// 4. If their guess matches the password, show them the secret message
-
+	if(c.equals(a)) {
+		JOptionPane.showMessageDialog(null, b+"");
+	} else {JOptionPane.showMessageDialog(null, "Intruder!!");}
 	// 5. If the password does not match, pop-up "INTRUDER!!"
 
-}
+}}
--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -2,24 +2,35 @@ package _07_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String a=JOptionPane.showInputDialog("What is 2+2?");
+		String b="4";
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		if(a.equals(b)) {
+			JOptionPane.showMessageDialog(null, "correct");
+			score=score+1;
+			} else{JOptionPane.showMessageDialog(null, "Wrong, answer=4!!!");}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
+		String c=JOptionPane.showInputDialog("What is 3*2?");
+		String d="6";
+		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		if(c.equals(d)) {
+			JOptionPane.showMessageDialog(null, "correct");
+			score=score+1;
+			} else{JOptionPane.showMessageDialog(null, "Wrong, answer=6!!!");}
 
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null, "score:"+score);
 	}
 }
 
